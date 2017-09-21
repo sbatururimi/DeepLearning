725b2de7c243f4b2d6f0d2f78fe485d2073e295a
Stas Batururimi <blueocean87@me.com>
2017-09-21 17:56:32 +0300

    Use  a dropout in decoding_layer_train

---
+++ language-translation/command.sh
@@ -0,0 +1,3 @@
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/ZsLynuU6ogaVF8JoumZEGT' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.iopub_data_rate_limit=1.0e10
